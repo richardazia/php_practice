2e36d59ec1fa842b697b8a1db26ec93e09509e11
Richard A <richard@main-vision.com>
2022-09-14 09:47:28 +0000

    array refresh

--- index.php
+++ index.php
@@ -7,28 +7,52 @@
       $age = 40;
       $name = 'Richard';
       $continent = 'Europe';
-      echo "Good Morning, $name, in $continent";
+      echo "<h3>Good Morning, $name, in $continent</h3>";
       echo 'Good Morning, ' . $name . 'in ' . $continent;
-      echo "\nThis is line 1\n";
-      echo "This is line 2\n";
+      echo "<p>This is line 1</p>";
+      echo "<p>This is line 2</p>";
       echo "\t this is line three, tabbed";
       echo "The quick brown fox
             jumped over the fence to 
             get to the lazy dog.";
-echo "This string spans
-multiple lines. The newlines will be
-output as well";
-echo "This string spans\nmultiple lines. The newlines will be\noutput as well.";
+      echo "This string spans
+            multiple lines. The newlines will be
+            output as well";
+      echo "This string spans\nmultiple lines. The                 newlines will be\noutput as well.";
 
+// Indexable Array
+        $colours = array('red', 'orange', 'green', 'blue', 'indigo', 'violet');
+
+      print_r( $colours);
+
+      echo '<p>' . $colours[0] . '<p>';
+      echo '<p>' . $colours[0] . ' ' . 'to '. $colours[5] . '<p>';
+      echo '<p>' . $colours . '<p>'; # prints "Array"
+
+      $colours[] = 'cyan';
+      $colours[] = 'azure blue';
+      $colours[] = 'white';
+echo '<pre>';
+print_r( $colours);
+echo '</pre>';
+      
+
+// Associative Array
+$week_day = array(
+  'Monday' => 'sunny',
+  'Tuesday' => 'rainy',
+  'Wednesday' => 'tropical heat',
+  'Thursday' => 'foggy',
+  'Friday' => 'windy and rainy',
+  'Saturday' => 'changeable weather',
+  'Sunday' => 'snowing',
+);
+
+echo '<pre>';
+print_r( $week_day );
+echo '</pre>';
 
   ?>
-    <?php echo '<h1>Hello World</h1>'?>
-    <?php echo '<h2>Good Morning</h2>'?>
-    <?php echo '<p>Hello World</p>'; ?> 
-    <?php echo $age ?>
-    <?php echo $name ?>
-    <?php echo $continent ?>
-
     <!--
     This script places a badge on your repl's full-browser view back to your repl's cover
     page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
