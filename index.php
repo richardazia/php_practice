036c3b3303356312b1d6b600769f106a54521f73
Richard A <richard@main-vision.com>
2022-09-14 08:12:14 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php 
+      $age = 40;
+      $name = 'Richard';
+      $continent = 'Europe';
+    ?>
+    <?php echo '<h1>Hello World</h1>'?>
+    <?php echo '<h2>Good Morning</h2>'?>
+    <?php echo '<p>Hello World</p>'; ?> 
+    <?php echo $age ?>
+    <?php echo $name ?>
+    <?php echo $continent ?>
+
+    <!--
+    This script places a badge on your repl's full-browser view back to your repl's cover
+    page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+    teal, blue, blurple, magenta, pink!
+    -->
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
