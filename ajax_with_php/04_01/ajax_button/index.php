0deeb61ed3153551cfe59210ee6940c9c99561ce
Richard Azia <richard@main-vision.com>
2022-10-06 17:35:10 +0200

    attemot to unfavourite

--- ajax_with_php/04_01/ajax_button/index.php
+++ ajax_with_php/04_01/ajax_button/index.php
@@ -3,6 +3,10 @@
 
   if(!isset($_SESSION['favorites'])) { $_SESSION['favorites'] = []; }
 
+  function is_favorite($id) {
+    return in_array($id, $_SESSION['favorites']);
+  }
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -29,22 +33,36 @@
         background: #000099;
       }
 
+      .favorite-heart {
+        color:  red;
+        font-size: 2em;
+        float: right;
+        display: none;
+      }
+
+      .favorite .favorite-heart {
+        display: block;
+      }
+
     </style>
   </head>
   <body>
     <?php echo join(', ', $_SESSION['favorites']); ?>
     <div id="blog-posts">
-      <div id="blog-post-101" class="blog-post">
+      <div id="blog-post-101" class="blog-post <?php if(is_favorite(101)) {echo 'favorite';} ?>">
+        <span class="favorite-heart">&hearts;</span>
         <h3>Blog Post 101</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
         <button class="favorite-button">Favorite</button>
       </div>
-      <div id="blog-post-102" class="blog-post">
+      <div id="blog-post-102" class="blog-post <?php if(is_favorite(102)) {echo 'favorite';} ?>">
+        <span class="favorite-heart">&hearts;</span>
         <h3>Blog Post 102</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
         <button class="favorite-button">Favorite</button>
       </div>
-      <div id="blog-post-103" class="blog-post">
+      <div id="blog-post-103" class="blog-post"  <?php if(is_favorite(103)) {echo 'favorite';} ?>>
+        <span class="favorite-heart">&hearts;</span>
         <h3>Blog Post 103</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
         <button class="favorite-button">Favorite</button>
@@ -58,6 +76,7 @@
 
         var xhr = new XMLHttpRequest();
         xhr.open('POST', '../favorite.php', true);
+        xhr.open('POST', '../favorite.php', false);
         // To ensure that this is sent as a post request: 
         xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
         xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
@@ -65,6 +84,13 @@
           if(xhr.readyState == 4 && xhr.status == 200) {
             var result = xhr.responseText;
             console.log('Result: ' + result);
+            if(result == 'true') {
+              parent.classList.add("favorite");
+            }
+            // my attempt
+            if(result == 'false') {
+              parent.classList.remove("favorite");
+            }
           }
         };
         // pass in the id of the element
