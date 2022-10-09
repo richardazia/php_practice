acdbf2a08cdeef2ed62d8dbb56a975f629a6f039
Richard Azia <richard@main-vision.com>
2022-10-09 12:49:30 +0200

    code looks correct but with a bug

--- ajax_with_php/07_01/ajax_autosuggest/autosuggest.php
+++ ajax_with_php/07_01/ajax_autosuggest/autosuggest.php
@@ -11,8 +11,37 @@
     exit;
   }
 
+  function str_starts_with($choice, $query) {
+    return strpos($choice, $query) === 0;
+  }
+
+  function str_contains($choice, $query) {
+    return strpos($choice, $query) !== false;
+  }
+
+
+  function search($query, $choices) {
+    $matches = [];
+
+    $d_query = strtolower($query);
+
+    foreach($choices as $choice) {
+      $d_choice = strtolower($choice);
+      if(str_starts_with($d_choice, $d_query)) {
+        $matches[] = $choice;
+      }
+    }
+
+    return $matches;
+  }
+
   $query = isset($_GET['q']) ? $_GET['q'] : '';
 
   // find and return search suggestions as JSON
+
+  $choices = file('https://github.com/dwyl/english-words/blob/60eb05a1285cae4ff306f0d9dcea407490cf2a99/words_alpha.txt');
   
+  $suggestions = search($query, $choices);
+
+  echo json_encode($suggestions);
 ?>
