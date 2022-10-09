fe61445950b38da72ab66d223b4ee4f997b03ad1
Richard Azia <richard@main-vision.com>
2022-10-09 10:51:37 +0200

    add self-refresh on scroll

--- ajax_with_php/06_01/ajax_scroll/index.php
+++ ajax_with_php/06_01/ajax_scroll/index.php
@@ -34,6 +34,7 @@
 
       var container = document.getElementById('blog-posts');
       var load_more = document.getElementById('load-more');
+      var request_in_progress = false;
 
       function showSpinner() {
         var spinner = document.getElementById("spinner");
@@ -72,7 +73,19 @@
         load_more.setAttribute('data-page', page);
       }
 
+      function scrollReaction() {
+        var content_height = container.offsetHeight;
+        var current_y = window.innerHeight + window.pageYOffset;
+        console.log(current_y + '/' + content_height);
+        if(current_y >= content_height) {
+          loadMore();
+        }
+      }
+
       function loadMore() {
+
+        if(request_in_progress) { return; }
+        request_in_progress = true;
 
         showSpinner();
         hideLoadMore();
@@ -93,6 +106,7 @@
             // append results to end of blog posts
             appendToDiv(container, result);
             showLoadMore();
+            request_in_progress = false;
 
           }
         };
@@ -100,6 +114,10 @@
       }
 
       load_more.addEventListener("click", loadMore);
+
+      window.onscroll = function() {
+        scrollReaction();
+      }
 
       // Load even the first page with Ajax
       loadMore();
