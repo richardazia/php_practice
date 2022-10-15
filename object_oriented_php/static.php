fad4ad61c7dd3a3eeb1126eef4113abea71d7772
Richard Azia <richard@main-vision.com>
2022-10-15 18:09:53 +0200

    inherited static behaviours

--- object_oriented_php/static.php
+++ object_oriented_php/static.php
@@ -4,7 +4,7 @@ echo "<h1>Static Modifiers</h1>";
 
 class Diver {
 
-  public static $level = ['bubble maker', 'openwater', 'openwater advanced', 'rescue diver', 'divemaster'];
+  public static $levels = ['bubble maker', 'openwater', 'openwater advanced', 'rescue diver', 'divemaster'];
   private static $total_students = 0;
 
   public static function motto() {
@@ -20,7 +20,7 @@ class Diver {
   }
 }
 
-echo Diver::$level[3] . "<br />";
+echo Diver::$levels[3] . "<br />";
 echo Diver::motto() . "<br />";
 
 // echo Diver::$total_students . "<br />"; // Uncaught Error: Access to undeclared static property Diver::$total_students
@@ -31,5 +31,29 @@ Diver::add_student();
 Diver::add_student();
 Diver::add_student();
 echo Diver::count() . "<br />";
+
+// Static methods and properties are inherited
+class SeasonalDiver extends Diver {
+
+}
+
+echo SeasonalDiver::$levels[0] . "<br />";
+echo SeasonalDiver::motto() . "<br />";
+
+// These are mutable, so changes to one affect all.
+
+SeasonalDiver::$levels[] = 'CMAS *';
+SeasonalDiver::$levels[] = 'CMAS **';
+SeasonalDiver::$levels[] = 'CMAS ***';
+echo implode(', ', Diver::$levels);
+echo "<br />";
+
+Diver::add_student();
+Diver::add_student();
+Diver::add_student();
+SeasonalDiver::add_student();
+echo Diver::count() . "<br />";
+echo SeasonalDiver::count() . "<br />";
+
 
  ?>
