8b2acd166da37c49e640e1348f9c3dfc7ed52372
Richard Azia <richard@main-vision.com>
2022-10-23 14:40:41 +0200

    count coin flips and outcomes

--- function.php
+++ function.php
@@ -1,15 +1,36 @@
 <?php
 
-echo "<h1>Function</h1>";
-$time_to_open_windows = 750;
-$current_reading = readline( 'What is the current reading? ');
-echo "<p>$current_reading</p>";
+// echo "<h1>Function</h1>";
+// $time_to_open_windows = 750;
+// $current_reading = readline( 'What is the current reading? ');
+// echo "<p>$current_reading</p>";
+//
+// if ( $current_reading <= $time_to_open_windows ) {
+//   echo "You can stay warm with closed windows but wear a mask";
+// } else {
+//   echo "Open the windows";
+// }
 
-if ( $current_reading <= $time_to_open_windows ) {
-  echo "You can stay warm with closed windows but wear a mask";
-} else {
-  echo "Open the windows";
+function coin_flips( $coin_flips ) {
+  $i = 0;
+  $heads = 0;
+  $tails = 0;
+
+  while( $i < $coin_flips ) {
+    if ( random_int(0,1) ) {
+      $heads++;
+    } else {
+      $tails++;
+    }
+    $i++;
+  }
+
+  echo "<p><b>Results:</b> $heads heads and $tails tails.</p>";
 }
+
+// coin_flips(50);
+
+coin_flips( readline( 'How many times do you want to coin?' ) );
 // doesn't work as desired
 // if ($current_reading >= $time_to_open_windows) {
 //   echo "open windows";
