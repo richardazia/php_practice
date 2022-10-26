1f860733102e1e9caaddd87c3151927e774aa3b3
Richard Azia <richard@main-vision.com>
2022-10-26 16:43:40 +0200

    browser mirror additions

--- single_serving_sites/find_ip.php
+++ single_serving_sites/find_ip.php
@@ -1,14 +1,23 @@
-// This code is copied or slightly modified from the Kevin Skoglund Course: Easy PHP projects, single-Serving Sites available on Linkedin Learning.
-
 <?php
 
+// This code is copied or slightly modified from the Kevin Skoglund Course: Easy PHP projects, single-Serving Sites available on Linkedin Learning.
+echo "<h1>Random server related information</h1>";
 echo "<pre>";
 print_r($_SERVER);
 echo "<pre>";
 
 echo "<p>Your IP address is: " . $_SERVER['REMOTE_ADDR'];
 echo "<p>Your remote port is: " . $_SERVER['REMOTE_PORT'];
+echo "<p>UNIX request time: " . $_SERVER['REQUEST_TIME'];
 
+date_default_timezone_set("Europe/Paris");
+echo "<p>The time is " . date("h:i:sa");
+
+echo "<h2>Browser screen info</h2>";
+echo "<p>Browser window width:<span id='window-width'></span></p>";
+echo "<p>Browser window width:<span id='window-height'></span></p>";
+
+echo "<h2>Forwarded ip</h2>";
 function forwarded_ip() {
 
   //Spoof forwarding
@@ -66,4 +75,24 @@ if(validate_ip('1.1.1.1,2.2.2.2,193.168.1.1')) {
   echo '<p>This is not a valid IP address';
 }
 
+if(isset($_SERVER['HTTPS'])) {
+  echo '<p>yes';
+} else {
+  echo '<p>Nope';
+}
+echo "<h2>Today</h2>";
+echo '<p>' . date('l, j F, Y g:ia', $_SERVER['REQUEST_TIME']);
+
 ?>
+
+<script>
+console.log("Test to see if it works");
+
+var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth || document.body.offsetWidth;
+
+var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight || document.body.offsetHeight;
+
+document.getElementById('window-width').innerHTML = width + "px";
+document.getElementById('window-height').innerHTML = height + "px";
+
+</script>
