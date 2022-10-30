89d5b227a7d1067264ede4fb2b9a9f36ced2f7c2
Richard Azia <richard@main-vision.com>
2022-10-30 11:42:07 +0100

    use iterator iterator

--- standard_library/directory.php
+++ standard_library/directory.php
@@ -1,6 +1,7 @@
 <?php
 // Learning the Standard PHP library
 $dir = new DirectoryIterator('../standard_library');
+$fs = new FilesystemIterator('../standard_library', FilesystemIterator::UNIX_PATHS);
 // foreach ($dir as $file) {
 foreach ($dir as $key => $file) {
   if (is_file($file)) {
@@ -16,3 +17,32 @@ foreach ($dir as $key => $file) {
 }
 
 echo "<p>Trying to see the array {$files[0]->getFilename()}";
+
+foreach ($fs as $key => $file) {
+  echo '<p>' . $key . '>> ' .$file . '<br>';
+  $files[] = $file;
+  if (is_file($file)) {
+    echo "<h1>Exploring Files and directories</h1>";
+    echo "<h2>Var Dump output</h2>";
+    echo "<pre>";
+    var_dump($file);
+    echo "<pre>";
+  }
+  echo $files[0]->getFilename();
+}
+
+
+
+echo "<p>Trying to see the array {$files[0]->getFilename()}";
+
+$recursive_files = new RecursiveDirectoryIterator('../standard_library');
+$recursive_files->setFlags(FilesystemIterator::SKIP_DOTS | FilesystemIterator::UNIX_PATHS);
+$recursive_files = new RecursiveIteratorIterator($recursive_files, RecursiveIteratorIterator::SELF_FIRST);
+$recursive_files->setMaxDepth(4);
+foreach ($recursive_files as $file) {
+  echo '<p>Unix path and Skip Dots ' . $file . '<br>';
+}
+
+
+
+?>
