933a4ab8bfed746fc594da17a3e79c0e1d8c364b
Richard Azia <richard@main-vision.com>
2022-10-31 10:35:15 +0100

    description of the course I am using

---
+++ standard_library/readme.php
@@ -0,0 +1,7 @@
+<?php 
+
+echo "<h1>Learning the Standard PHP Library</h1>";
+echo "<p>Course by David Powers</p>";
+echo "<hr>";
+echo "<p>Rather than just use the course files I write the code that you see here line by line to understand it.</p>
+
