cad7efd90588afe7418af50db12454441e6f1f84
Richard Azia <richard@main-vision.com>
2022-11-15 12:39:59 +0100

    routing with php

---
+++ php_framework/index.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'Router.php';
+
+$router = new Router();
+
+// echo get_class($router);
+
+// The routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+// echo '<pre>';
+// var_dump($router->getRoutes());
+// echo '</pre>';
+
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+  echo '<pre>';
+  var_dump($router->getParams());
+  echo '</pre>';
+} else {
+  echo "No route found for URL '$url'";
+}
+
+?>
